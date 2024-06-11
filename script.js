a6b5a9b553ad3276454dec0205d9791514ba880d
Fungari1 <106318944+Fungari1@users.noreply.github.com>
2024-06-11 10:52:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,51 @@ const caixaPerguntas = document.querySelect(".caixa-perguntas")
 const caixaAlternativas = document.querySelect(".caixa-alternativas")
 const caixaResultado = document.querySelect(".caixa-resultado")
 const textoResultado = document.querySelect(".texto-resultado")
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
